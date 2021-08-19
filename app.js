845cf8f59f2eab196b8c56afa640a2440fa0f97a
Hack-Light <onohsomtochukwubasil@gmail.com>
2021-08-19 20:18:23 +0100

    First upload

---
+++ app.js
@@ -0,0 +1,52 @@
+// var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+// var cookieParser = require("cookie-parser");
+// var logger = require("morgan");
+
+var indexRouter = require("./routes/indexRouter");
+var teamsRouter = require("./routes/teamsRouter");
+var authRouter = require("./routes/authRouter");
+
+var app = express();
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/css", express.static(path.join(__dirname, "/public/stylesheets")));
+app.use("/js", express.static(path.join(__dirname, "/public/javascripts")));
+app.use("/img", express.static(path.join(__dirname, "/public/images")));
+app.use("/vendor", express.static(path.join(__dirname, "/public/vendor")));
+
+app.use("/", indexRouter);
+app.use("/teams", teamsRouter);
+app.use("/auth", authRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+app.listen(3800, () => {
+  console.log("listening on port 3000");
+});
+
+module.exports = app;
